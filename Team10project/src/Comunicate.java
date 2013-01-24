7c59b3e390defe6e2dfa2dd7913cd4a3e30bc67e
gavin.lluttrell.williams@gmail.com <gavin.lluttrell.williams@gmail.com@5ad5377b-60a1-7cf3-9ad4-50bd737fd532>
2013-01-24 07:21:49 +0000

    I've modified the comuniacte file in to a class that shoudl allow for an instance to communicate with the server. it includes an initialize, send recive and terminate methods. i will soon hopfully include a file that we can use for testing this class

--- Team10project/src/Comunicate.java
+++ Team10project/src/Comunicate.java
@@ -1,19 +1,19 @@
 	import java.io.*;
 	import java.net.*;
-	public class Comunicate {
-	    public static void main(String[] args) {
-	// declaration section:
-	// smtpClient: our client socket
-	// os: output stream
-	// is: input stream
-	        Socket smtpSocket = null;  
-	        DataOutputStream os = null;
-	        DataInputStream is = null;
-	// Initialization section:
-	// Try to open a socket on port 25
-	// Try to open input and output streams
+public class Comunicate {
+    private Socket smtpSocket = null;  
+    private DataOutputStream os = null;
+    private DataInputStream is = null;
+    public void initiate(String host) {
+	/* declaration section:
+	 * smtpClient: our client socket
+	 * os: output stream
+	 * is: input stream
+	 * Initialization section:
+	 * Try to open a socket on port 9999
+	 */
 	        try {
-	            smtpSocket = new Socket("10.11.17.89", 9999);
+	            smtpSocket = new Socket(host, 9999);
 	            os = new DataOutputStream(smtpSocket.getOutputStream());
 	            is = new DataInputStream(smtpSocket.getInputStream());
 	        } catch (UnknownHostException e) {
@@ -23,41 +23,52 @@
 	        }
 	// If everything has been initialized then we want to write some data
 	// to the socket we have opened a connection to on port 25
+	    }
+	boolean send_message(String message){
 	    if (smtpSocket != null && os != null && is != null) {
 	            try {
-	// The capital string before each colon has a special meaning to SMTP
-	// you may want to read the SMTP specification, RFC1822/3
-	        os.writeBytes("HELO\n");    
-	                os.writeBytes("MAIL From: k3is@fundy.csd.unbsj.ca\n");
-	                os.writeBytes("RCPT To: k3is@fundy.csd.unbsj.ca\n");
-	                os.writeBytes("DATA\n");
-	                os.writeBytes("From: k3is@fundy.csd.unbsj.ca\n");
-	                os.writeBytes("Subject: testing\n");
-	                os.writeBytes("Hi there\n"); // message body
-	                os.writeBytes("\n.\n");
-	        os.writeBytes("QUIT");
-	// keep on reading from/to the socket till we receive the "Ok" from SMTP,
-	// once we received that then we want to break.
-	                String responseLine;
-	                while ((responseLine = is.readLine()) != null) {
-	                    System.out.println("Server: " + responseLine);
-	                    if (responseLine.indexOf("Ok") != -1) {
-	                      break;
-	                    }
-	                }
-	// clean up:
-	// close the output stream
-	// close the input stream
-	// close the socket
-	        os.close();
-	                is.close();
-	                smtpSocket.close();   
+			        os.writeBytes(message);    
+			        os.writeBytes("END");
+			        return true;
 	            } catch (UnknownHostException e) {
 	                System.err.println("Trying to connect to unknown host: " + e);
+	                return false;
 	            } catch (IOException e) {
 	                System.err.println("IOException:  " + e);
+	                return false;
+	            }    
+	// keep on reading from/to the socket till we receive the "Ok" from SMTP,
+	// once we received that then we want to break.
+	    }
+		return false;
+	}
+	String recive_message(){
+        String responseLine = null;
+		try{
+	        while ((responseLine = is.readLine()) != null) {
+	            System.out.println("Server: " + responseLine);
+	            if (responseLine.indexOf("Ok") != -1) {
+	              break;
 	            }
 	        }
-	    }           
+		}catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+        return responseLine;
 	}
+void disconect(){
+	try{
+    os.close();
+    is.close();
+    smtpSocket.close();  
+	}catch (UnknownHostException e) {
+        System.err.println("Trying to connect to unknown host: " + e);
+    } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+    }
+	
+	}
+}
 
