dd37d884af8750fe752f7c33176d216cb939682b
r.gavard@gmail.com <r.gavard@gmail.com@5ad5377b-60a1-7cf3-9ad4-50bd737fd532>
2013-01-29 22:43:23 +0000

    Test for a window

---
+++ Team10project/src/drawboard.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+public class drawboard {
+public static void main(String[] args){
+	JFrame fenetre = new JFrame();
+
+		fenetre.setTitle("First window");
+
+
+		fenetre.setSize(400, 500);
+
+		fenetre.setLocationRelativeTo(null);
+
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setVisible(true);
+	}
+}
