539172e20e15d0820f9464d77189cb4d6d1f9bae
r.gavard@gmail.com <r.gavard@gmail.com@5ad5377b-60a1-7cf3-9ad4-50bd737fd532>
2013-02-05 04:13:16 +0000

    now the board is in 10X10

--- trunk/Team10project/src/drawwindow/drawOnWindow.java
+++ trunk/Team10project/src/drawwindow/drawOnWindow.java
@@ -5,27 +5,24 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class drawOnWindow extends JPanel {
-	private int X_UP = 175;
-	private int Y_UP = 75;
+	private int middleX = 400;
+	private int middleY = 300;
 	
-	private int X_DOWN = 625;
-	private int Y_DOWN = 525;
 	
 	public void paintComponent(Graphics g){
-
+		int X_UP = middleX -250;
+		int Y_UP = middleY -250;
+	
+		int X_DOWN = middleX + 250;
+		int Y_DOWN = middleY + 250;
 			System.out.println("That's an awesome test ! ! !");
-			for (int i=0; i<10 ; i++) {
+			for (int i=0; i<11 ; i++) {
 				g.drawLine(X_UP, Y_UP+i*50, X_DOWN, Y_UP+i*50);
 			}
-			for (int i=0; i<10 ; i++) {
+			for (int i=0; i<11 ; i++) {
 				g.drawLine(X_UP+i*50, Y_UP, X_UP+i*50, Y_DOWN);
 			}
-			for (int i=0; i<4 ; i++) {
-				g.drawLine(X_UP, Y_UP+i*150, X_DOWN, Y_UP+i*150);
-			}
-			for (int i=0; i<4 ; i++) {
-				g.drawLine(X_UP+i*150, Y_UP, X_UP+i*150, Y_DOWN);
-			}
+			
 			
 	}
 }
