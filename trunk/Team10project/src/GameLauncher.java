6cf65f2ced2275f4316fee3fa31fe849d230543a
Evolution_Terry@hotmail.com <Evolution_Terry@hotmail.com@5ad5377b-60a1-7cf3-9ad4-50bd737fd532>
2013-02-14 17:48:26 +0000

    Updated with multiple players

--- trunk/Team10project/src/GameLauncher.java
+++ trunk/Team10project/src/GameLauncher.java
@@ -7,27 +7,50 @@ public class GameLauncher{
 	static Scanner keyboard = new Scanner(System.in);
 	
 	public static void main(String[] args){		
-		//Player Winner = new Player();
+		Player Winner = new Player();
 		
-		//Asks user for the number of players, and converts it to an int
+		//Asks user for the number of players, and converts it to an integer
 		System.out.println("Please enter the number of players: ");
 		String numberOfPlayersStr = keyboard.nextLine();
 		int numberOfPlayers = Integer.parseInt(numberOfPlayersStr);
+		
+		Player[] Player;
+		Player = new Player[numberOfPlayers];
+		
+		
 		
 		//Sets a name for each number entered
 		for (int i = 0; i < numberOfPlayers; i++) {
 			System.out.println("Please enter player " + (i + 1) + "'s name: ");
 			String someName = keyboard.nextLine();
-			Player player = new Player();
-			player.setName(someName + i);
+			Player[i] = new Player();
+			Player[i].setName(someName);
 		}
-		/*while (Winner.getName() == null) {
+		int target;
+		int x;
+		int y;
+		
+		while (Winner.getName() == null) {
+			for (int i = 0; i < numberOfPlayers-1; i++) {
+			System.out.println("It is " + Player[i].getName() + "'s turn!");
 			
-			// Play the game!
-			System.out.println("There is no winner!");
+			System.out.println("Enter the player number you want to attack:");
+			target = keyboard.nextInt();
+			
+			System.out.println("Enter the x-coordinate you want to attack:");
+			x = keyboard.nextInt();
+			
+			System.out.println("Enter the y-coordinate you want to attack:");
+			y = keyboard.nextInt();
+			
+			
+			Player[i].attack(Player[target], x, y);
+			System.out.println("There is no winner!");			
+			
+			}
 		}
 		System.out.println("The winner is: " + Winner.getName());
-		*/
+		
 		
 		
 		
